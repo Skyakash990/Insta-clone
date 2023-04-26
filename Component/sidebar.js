55c46bc2e38e3c9bac378cbe27967a9cdc44458b
Akash Nandoliya <nandoliyaakas55@gmail.com>
2023-04-26 18:32:05 +0530

    akki

---
+++ Component/sidebar.js
@@ -0,0 +1,20 @@
+const sidebar=`  <div class="side pt-5 border-r-[0.5px] border-gray-300  w-[14.5rem]">
+<img src="./Assets/Img/1522452762Instagram-logo-png-text.png" class="w-[8.5rem]  p-5"  alt="">
+<div class="sidemenu ps-6 pt-5">
+    <ul class="ul flex flex-col">
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-solid fa-house fa-lg" style="color: #000000;"></i>Home</a></li>
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-solid fa-magnifying-glass fa-lg" style="color: #000000;"></i>Search</a></li>
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-regular fa-compass fa-lg" style="color: #000000;"></i>Explore</a></li>
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-solid fa-clapperboard fa-lg" style="color: #000000;"></i>Reels</a></li>
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-regular fa-comment-dots fa-lg" style="color: #000000;"></i>Messages</a></li>
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-regular fa-heart fa-lg" style="color: #000000;"></i>Notifications</a></li>
+        <li class="font-normal pb-9"><a href=""><i class="w-10 fa-regular fa-square-plus fa-lg" style="color: #000000;"></i>Create</li>
+        <li class="font-normal pb-9"><img src="." class="w-8" alt=""> Profile</li>
+    </ul>
+</div>
+<div class="bottom-bar">
+<button class="ps-5 pt-20"><i class="fa-solid w-12 fa-light fa-bars fa-xl" style="color: #000000;"></i>More</button>
+</div>
+</div>
+`
+export default sidebar;
---
+++ script/script.js
@@ -0,0 +1,3 @@
+import sidebar from "../Component/sidebar";
+const side=document.getElementById("sidebar");
+side.innerHTML=sidebar;
